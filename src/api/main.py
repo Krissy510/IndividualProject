28a4b462004ad250834256919ef6465877340b68
Kris Raviphas <2827932s@student.gla.ac.uk>
2023-11-13 22:22:02 +0000

    Add Index variant validation

--- src/api/main.py
+++ src/api/main.py
@@ -7,7 +7,7 @@ import pyterrier as pt
 from fastapi import FastAPI, HTTPException, status
 
 import validate as val
-from model import RetrieveRequest
+from model import *
 
 if not pt.started():
     pt.init()
@@ -23,6 +23,9 @@ def terrierRetreive(request: RetrieveRequest):
     if (not val.wmodel(request.wmodel)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid wmodel")
+    if (not val.variant(request.index_variant)):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid index variant")
     pipeline = pt.BatchRetrieve.from_dataset(
         request.dataset, request.index_variant, wmodel=request.wmodel)
     result = pipeline(request.queries)
--- src/api/validate.py
+++ src/api/validate.py
@@ -11,3 +11,9 @@ def wmodel(wmodel_name: str) -> bool:
                      'InB2', 'InL2', 'Js_KLs', 'LemurTF_IDF', 'LGD', 'MDL2', 'ML2', 'Null', 'PL2',
                      'TF_IDF', 'XSqrA_M'}
     return wmodel_name in valid_wmodels
+
+
+def variant(index_variant: str) -> bool:
+    valid_index_variant = {'terrier_stemmed', 'terrier_stemmed_positions',
+                           'terrier_unstemmed', 'terrier_stemmed_text', 'terrier_unstemmed_text'}
+    return index_variant in index_variant
