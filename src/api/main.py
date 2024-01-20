5c3d2cc6a75bbc0382f33a42212c27791c4d5c40
Kris Raviphas <Kris5103@outlook.com>
2024-01-20 12:06:29 +0000

    Change initialization to use helper func

--- src/api/main.py
+++ src/api/main.py
@@ -5,11 +5,12 @@ import pyterrier as pt
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from helper import pyterrier_init
 from explanationIllustrations import working_text
 from gettingStarted import query_rewrite_and_expansion, retrival
 
-if not pt.started():
-    pt.init()
+pyterrier_init()
+
 
 app = FastAPI()
 
