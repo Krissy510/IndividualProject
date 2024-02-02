878f685fd28f35fee1e083ec6240cb8d18fb852f
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-02-02 19:58:18 +0000

    Refactor pisa index generator

--- src/api/index_generate.py
+++ src/api/index_generate.py
@@ -7,6 +7,5 @@ pyterrier_init()
 indexer = pt.IterDictIndexer('./vaswani.terrier')
 indexer.index(pt.get_dataset('irds:vaswani').get_corpus_iter())
 
-dataset_pisa = pt.get_dataset('irds:antique/test')
-idx = PisaIndex('./pisa-antique-index')
-idx.index(dataset_pisa.get_corpus_iter())
+pisa_idx = PisaIndex('./antique.pisa')
+pisa_idx.index(pt.get_dataset('irds:antique/test').get_corpus_iter())
