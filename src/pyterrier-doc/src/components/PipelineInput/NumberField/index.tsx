8ba1bfcbdee5b7b2b9ebbbd977fa83e2777b6154
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-02-07 12:45:29 +0000

    Fix dark mode issue with parameters

--- src/pyterrier-doc/src/components/PipelineInput/NumberField/index.tsx
+++ src/pyterrier-doc/src/components/PipelineInput/NumberField/index.tsx
@@ -1,24 +1,45 @@
-import { TextField } from "@mui/material";
+import { Box, TextField, createTheme, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { useColorMode } from "@docusaurus/theme-common";
+
+// Custom theme for dark mode
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#ffffff", // White text color
+    },
+    primary: {
+      main: "#ffffff", // White border color
+    },
+  },
+});
 
 export function NumberField({ data, onChange, setParamValidity }) {
   const [value, setValue] = useState(data.default);
   const isError = !(value > 0);
+  const { colorMode, setColorMode } = useColorMode();
+  const theme = colorMode === "dark" ? darkTheme : createTheme(); // Use dark theme if prefers dark mode, otherwise use default theme
 
   return (
-    <TextField
-      type="number"
-      required
-      label={data.name}
-      onChange={(e) => {
-        const newValue = Number(e.target.value);
-        onChange(data.id, newValue);
-        setValue(newValue);
-        setParamValidity((prev) => ({ ...prev, [data.id]: newValue > 0 }));
-      }}
-      defaultValue={data.default}
-      error={isError}
-      helperText={isError ? "Value must be greater than 0" : ""}
-    />
+    <ThemeProvider theme={theme}>
+      <Box>
+        <span>{data.name}</span>
+        <TextField
+          type="number"
+          required
+          onChange={(e) => {
+            const newValue = Number(e.target.value);
+            onChange(data.id, newValue);
+            setValue(newValue);
+            setParamValidity((prev) => ({ ...prev, [data.id]: newValue > 0 }));
+          }}
+          defaultValue={data.default}
+          error={isError}
+          helperText={isError ? "Value must be greater than 0" : ""}
+          fullWidth
+        />
+      </Box>
+    </ThemeProvider>
   );
 }
--- src/pyterrier-doc/src/components/PipelineInput/SelectField/index.tsx
+++ src/pyterrier-doc/src/components/PipelineInput/SelectField/index.tsx
@@ -1,28 +1,51 @@
-import { Box, MenuItem, TextField } from "@mui/material";
+import {
+  Box,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { IColumns } from "../model";
+import { useColorMode } from "@docusaurus/theme-common";
 
 interface SelectField {
   data: IColumns;
   onChange: any;
 }
 
+// Custom theme for dark mode
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#ffffff", // White text color
+    },
+    primary: {
+      main: "#ffffff", // White border color
+    },
+  },
+});
+
 export function SelectField({ data, onChange }) {
+  const { colorMode, setColorMode } = useColorMode();
+  const theme = colorMode === "dark" ? darkTheme : createTheme(); // Use dark theme if prefers dark mode, otherwise use default theme
   return (
-    <Box>
-      <TextField
-        onChange={(e) => onChange(data.id, e.target.value)}
-        defaultValue={data.default}
-        label={data.name}
-        select
-        fullWidth
-        disabled={data.read_only}
-      >
-        {data.choices.map((choice, index) => (
-          <MenuItem key={index} value={choice}>
-            {choice}
-          </MenuItem>
-        ))}
-      </TextField>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <span>{data.name}</span>
+        <Select
+          onChange={(e) => onChange(data.id, e.target.value)}
+          defaultValue={data.default}
+          fullWidth
+          disabled={data.read_only}
+        >
+          {data.choices.map((choice, index) => (
+            <MenuItem key={index} value={choice}>
+              {choice}
+            </MenuItem>
+          ))}
+        </Select>
+      </Box>
+    </ThemeProvider>
   );
 }
