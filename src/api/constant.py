ea82480e254c9842c9db092bf590c060c51aaa77
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-02-12 16:11:13 +0000

    Move constant from generate to constant.py

--- src/api/constant.py
+++ src/api/constant.py
@@ -28,3 +28,143 @@ idx = PisaIndex('./pisa-antique-index')
 idx.index(dataset_pisa.get_corpus_iter())''',
     'none': ''
 }
+
+# Define a mapping of field names to their widths
+FIELD_WIDTHS = {
+    'qid': 30,
+    'query': 200,
+    'query_0': 200,
+    'docno': 80,
+    'body': 300,
+    'text': 300,
+    'score': 80,
+    'rank': 30,
+    'stashed_results_0': 300,
+    'query_vec': 300,
+    'doc_vec': 300,
+}
+
+PRESET_PARAMETERS = {
+    'dataset': {
+        'name': 'Dataset',
+        'type': 'select',
+        'default': 'vaswani',
+        'id': 'dataset',
+        'choices': VALID_DATASET,
+        'read_only': True,
+    },
+    'wmodel': {
+        'name': 'Wmodel',
+        'type': 'select',
+        'default': 'BM25',
+        'id': 'wmodel',
+        'choices': VALID_WMODEL,
+    },
+    'index_variant': {
+        'name': 'Index Variant',
+        'type': 'select',
+        'default': 'terrier_stemmed',
+        'id': 'index_variant',
+        'choices': VALID_INDEX_VARIANT,
+    },
+    'num_results': {
+        'name': 'Num of result',
+        'type': 'number',
+        'default': 5,
+        'id': 'num_results',
+    },
+    'length': {
+        'name': 'Length',
+        'type': 'number',
+        'default': 5,
+        'id': 'length',
+    },
+    'stride': {
+        'name': 'Stride',
+        'type': 'number',
+        'default': 1,
+        'id': 'stride',
+    },
+    'fb_terms': {
+        'name': 'fb_terms',
+        'type': 'number',
+        'default': 10,
+        'id': 'fb_terms'
+    },
+    'fb_docs': {
+        'name': 'fb_docs',
+        'type': 'number',
+        'default': 3,
+        'id': 'fb_docs'
+    },
+    'fb_lambda': {
+        'name': 'fb_lambda',
+        'type': 'number',
+        'default': 0.6,
+        'id': 'fb_lambda'
+    },
+    'batch_size': {
+        'name': 'batch_size',
+        'type': 'number',
+        'default': 4,
+        'id': 'batch_size'
+    },
+    'k1': {
+        'name': 'k1',
+        'type': 'number',
+        'default': 1.2,
+        'id': 'k1'
+    },
+    'b': {
+        'name': 'b',
+        'type': 'number',
+        'default': 0.4,
+        'id': 'b'
+    },
+    'c': {
+        'name': 'c',
+        'type': 'number',
+        'default': 1.0,
+        'id': 'c'
+    },
+    'mu': {
+        'name': 'mu',
+        'type': 'number',
+        'default': 1000,
+        'id': 'mu'
+    },
+    'qre_dataset': {
+        'name': 'Dataset',
+        'type': 'select',
+        'default': 'vaswani',
+        'id': 'qre_dataset',
+        'choices': ['vaswani'],
+        'read_only': True
+    },
+    'pisa_dataset': {
+        'name': 'Dataset',
+        'type': 'select',
+        'default': 'antique',
+        'id': 'pisa_dataset',
+        'choices': ['antique'],
+        'read_only': True
+    },
+    'dr_model': {
+        'name': 'Model',
+        'type': 'select',
+        'default': 'castorini/tct_colbert-msmarco',
+        'id': 'model',
+        'choices': ['castorini/tct_colbert-msmarco'],
+        'read_only': True
+
+    },
+    'dr_ance_model': {
+        'name': 'Model',
+        'type': 'select',
+        'default': 'sentence-transformers/msmarco-roberta-base-ance-firstp',
+        'id': 'model',
+        'choices': ['sentence-transformers/msmarco-roberta-base-ance-firstp'],
+        'read_only': True
+
+    }
+}
--- src/api/generate.py
+++ src/api/generate.py
@@ -1,149 +1,37 @@
+"""
+This Python module serves as a utility for generating props used in documentation and API response JSON for frontend applications.
+
+The module includes functions for generating base code, index code, columns, parameters, and interactive props. It also provides a function for generating API response with result data, input details, and generated code.
+
+The key components and functionalities of this module are as follows:
+
+1. **Base Code Generation:**
+   - `generate_base_code(base_template_name: str)`: Generates base code using a specified template name.
+
+2. **Index Code Generation:**
+   - `generate_index_code(index_template_name: str)`: Generates index code using a specified template name.
+
+3. **Columns and Parameters Generation:**
+   - `generate_columns(cls: type) -> List[IColumns]`: Generates a list of columns with names and widths from the provided class.
+   - `generate_parameters(cls: type) -> List[IParameters]`: Generates a list of parameters with details from the provided class.
+
+4. **Interactive Props Generation:**
+   - `generate_interactive_props(example: List[dict], requestClass: type, outputClass: type)`: Generates interactive props including examples, input columns, output columns, and parameters.
+   - `generate_multi_interactive_props(optionsName: List[str], defaultOption: str, examples: List[List[dict]], requestClasses: List[type], outputClasses: List[type], parameters: List[str])`: Generates interactive props for multiple options, including examples, input columns, output columns, and parameters.
+
+5. **API Response Generation:**
+   - `generate_api_response(result: List, input: List, pipeline: str, base_template: str = 'none', index_template: str = 'default') -> ApiResponse`: Generates API response including result data, input details, and generated code.
+
+Note: The module also includes imports, constants, and presets necessary for its functioning.
+
+Make sure to review and customize the PRESET_PARAMETERS dictionary according to your specific requirements before using the module in your project.
+"""
+
 from enum import Enum
 from typing import Dict, List, Tuple, TypedDict, get_type_hints
 
 from model import *
 from constant import *
-
-# Define a mapping of field names to their widths
-field_widths = {
-    'qid': 30,
-    'query': 200,
-    'query_0': 200,
-    'docno': 80,
-    'body': 300,
-    'text': 300,
-    'score': 80,
-    'rank': 30,
-    'stashed_results_0': 300,
-    'query_vec': 300,
-    'doc_vec': 300,
-}
-
-preset_parameters = {
-    'dataset': {
-        'name': 'Dataset',
-        'type': 'select',
-        'default': 'vaswani',
-        'id': 'dataset',
-        'choices': VALID_DATASET,
-        'read_only': True,
-    },
-    'wmodel': {
-        'name': 'Wmodel',
-        'type': 'select',
-        'default': 'BM25',
-        'id': 'wmodel',
-        'choices': VALID_WMODEL,
-    },
-    'index_variant': {
-        'name': 'Index Variant',
-        'type': 'select',
-        'default': 'terrier_stemmed',
-        'id': 'index_variant',
-        'choices': VALID_INDEX_VARIANT,
-    },
-    'num_results': {
-        'name': 'Num of result',
-        'type': 'number',
-        'default': 5,
-        'id': 'num_results',
-    },
-    'length': {
-        'name': 'Length',
-        'type': 'number',
-        'default': 5,
-        'id': 'length',
-    },
-    'stride': {
-        'name': 'Stride',
-        'type': 'number',
-        'default': 1,
-        'id': 'stride',
-    },
-    'fb_terms': {
-        'name': 'fb_terms',
-        'type': 'number',
-        'default': 10,
-        'id': 'fb_terms'
-    },
-    'fb_docs': {
-        'name': 'fb_docs',
-        'type': 'number',
-        'default': 3,
-        'id': 'fb_docs'
-    },
-    'fb_lambda': {
-        'name': 'fb_lambda',
-        'type': 'number',
-        'default': 0.6,
-        'id': 'fb_lambda'
-    },
-    'batch_size': {
-        'name': 'batch_size',
-        'type': 'number',
-        'default': 4,
-        'id': 'batch_size'
-    },
-    'k1': {
-        'name': 'k1',
-        'type': 'number',
-        'default': 1.2,
-        'id': 'k1'
-    },
-    'b': {
-        'name': 'b',
-        'type': 'number',
-        'default': 0.4,
-        'id': 'b'
-    },
-    'c': {
-        'name': 'c',
-        'type': 'number',
-        'default': 1.0,
-        'id': 'c'
-    },
-    'mu': {
-        'name': 'mu',
-        'type': 'number',
-        'default': 1000,
-        'id': 'mu'
-    },
-    'qre_dataset': {
-        'name': 'Dataset',
-        'type': 'select',
-        'default': 'vaswani',
-        'id': 'qre_dataset',
-        'choices': ['vaswani'],
-        'read_only': True
-    },
-    'pisa_dataset': {
-        'name': 'Dataset',
-        'type': 'select',
-        'default': 'antique',
-        'id': 'pisa_dataset',
-        'choices': ['antique'],
-        'read_only': True
-    },
-    'dr_model': {
-        'name': 'Model',
-        'type': 'select',
-        'default': 'castorini/tct_colbert-msmarco',
-        'id': 'model',
-        'choices': ['castorini/tct_colbert-msmarco'],
-        'read_only': True
-
-    },
-    'dr_ance_model': {
-        'name': 'Model',
-        'type': 'select',
-        'default': 'sentence-transformers/msmarco-roberta-base-ance-firstp',
-        'id': 'model',
-        'choices': ['sentence-transformers/msmarco-roberta-base-ance-firstp'],
-        'read_only': True
-
-    }
-}
-
 
 def generate_base_code(base_template_name: str):
     base = BASE_TEMPLATES[base_template_name]
@@ -163,17 +51,17 @@ def generate_index_code(index_template_name: str):
 
 
 def generate_columns(cls: type) -> List[IColumns]:
-    return [{'name': field_name, 'width': field_widths[field_name]}
+    return [{'name': field_name, 'width': FIELD_WIDTHS[field_name]}
             for field_name in (get_type_hints(cls).keys())]
 
 
 def generate_parameters(cls: type) -> List[IParameters]:
     parameters = list(get_type_hints(cls).keys())
     parameters.remove('input')
-    return [preset_parameters[parameter] for parameter in parameters]
+    return [PRESET_PARAMETERS[parameter] for parameter in parameters]
 
 
-def generate_interactive_props(example: List[dict], requestClass: type, outputClass: type):
+def generate_interactive_props(example: List[dict], requestClass: type, outputClass: type) -> InteractiveFeatureProps:
     type_hints = get_type_hints(requestClass)
     query_type = type_hints['input'].__args__[0]
     return {
@@ -195,7 +83,7 @@ def generate_multi_interactive_props(optionsName: List[str], defaultOption: str,
             'output': generate_columns(outputClasses[i]),
         }
 
-    parameter = [preset_parameters[parameter] for parameter in parameters]
+    parameter = [PRESET_PARAMETERS[parameter] for parameter in parameters]
     parameter.append({
         'name': 'Type',
         'type': 'select',
