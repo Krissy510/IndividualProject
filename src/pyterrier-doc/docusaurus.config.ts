ff9ee000a84eaa8c1c5d8e3011601a9601404bb3
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-02-18 23:41:35 +0000

    Correct config

--- src/pyterrier-doc/docusaurus.config.ts
+++ src/pyterrier-doc/docusaurus.config.ts
@@ -15,15 +15,14 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Krissy510', // Usually your GitHub org/user name.
+  projectName: 'PyTerrier', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  // useful metadata like html lang.
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -38,14 +37,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Krissy510/IndividualProject/tree/main/src/pyterrier-doc/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Krissy510/IndividualProject/tree/main/src/pyterrier-doc/',
         },
         theme: {
           customCss: './src/css/custom.css',
