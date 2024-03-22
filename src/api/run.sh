8fd21054ff6efe076e6c80c9a7d717454a6075bd
Kris Raviphas <2827932s@student.gla.ac.uk>
2024-03-22 08:47:14 +0000

    Add start command to run script

--- src/api/run.sh
+++ src/api/run.sh
@@ -2,11 +2,13 @@
 
 if [ "$1" == "dev" ]; then
     uvicorn main:app --host 0.0.0.0 --port 8080 --reload
+elif [ "$1" == "start" ]; then
+    uvicorn main:app --host 0.0.0.0 --port 8080
 elif [ "$1" == "test" ]; then
     python -m unittest discover -s tests/unit
 elif [ "$1" == "init" ]; then
     pip install --no-cache-dir --upgrade -r requirements.txt
     python index_generate.py
 else
-    echo "Invalid command. Usage: ./run.sh [dev|test|init]"
+    echo "Invalid command. Usage: ./run.sh [dev|start|test|init]"
 fi
